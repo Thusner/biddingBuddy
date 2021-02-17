2d4f34ac680653a514dc62b4a61971ecc742daf6
Thusner <74198817+Thusner@users.noreply.github.com>
2021-02-17 21:06:37 +0100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,34 @@
+from flask import Flask, render_template,request
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/contact', methods = ["GET","POST"])
+def contactFunction():
+
+    if request.method == "POST":
+        #Get the data of the form from frontend
+
+        # Call the ai function (parameter, parameter 2)
+
+        # Show on your webpage the result from the AI
+
+        aiResults = aiFunction()
+
+        return render_template("resultPage.html", results = aiResults)
+
+
+    return render_template("contact.html")
+
+
+def aiFunction():
+    # Call the function you coded
+    return " What you got from the function"
+
+
+if __name__ == '__main__':
+    app.run()
