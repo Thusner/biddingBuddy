d1ffac220ef6f90dfcc1bb88d534b879e45b3581
Thusner <74198817+Thusner@users.noreply.github.com>
2021-02-18 13:52:11 +0100

    Update ourApi.py

--- ourApi.py
+++ ourApi.py
@@ -1,5 +1,6 @@
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template,request
+import algorithm as alg
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -9,18 +10,14 @@ app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 def main():
     return render_template("index.html")
 
-@app.route('/results', methods = ["GET","POST"])
 def contactFunction():
 
     if request.method == "POST":
         #Get the data of the form from frontend
-
         # Call the ai function (parameter, parameter 2)
-
         # Show on your webpage the result from the AI
 
         aiResults = aiFunction()
-
         return render_template("resultPage.html", results = aiResults)
 
 
